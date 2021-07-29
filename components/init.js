3d35f217a7008372d1f1f4fc5a0b121d02c64dc7
Iago Visie <iago.almeida@visie.com.br>
2021-07-29 13:07:06 -0300

    Mouse dragging

--- components/init.js
+++ components/init.js
@@ -22,12 +22,47 @@ class Game {
 class Player {
     createPlayer(position_x, position_y) {
         const player = PIXI.Sprite.from('assets/sprites/player.png');
+        
+        player.scale.set(1);
+
         player.anchor.set(0.5);
         player.x = position_x;
         player.y = position_y;
+
+        player.interactive = true;
+        player.buttonMode = true;
+
+        player
+        .on('pointerdown',      onDragStart)
+        .on('pointerup',        onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove',      onDragMove)
+
         return player;
     }
 }
+
+
+function onDragStart(event) {
+    this.data = event.data;
+    this.alpha = 0.5;
+    this.dragging = true;
+}
+
+function onDragEnd() {
+    this.alpha = 1;
+    this.dragging = false;
+    this.data = null;
+}
+
+function onDragMove() {
+    if(this.dragging) {
+        const newPosition = this.data.getLocalPosition(this.parent);
+        this.x = newPosition.x;
+        this.y = newPosition.y;
+    }
+}
+
 
 function main() {
     const HTML_DOM = document.getElementById('game');
@@ -39,7 +74,12 @@ function main() {
     const center_position_y = game.app.renderer.height / 2;
     const hero = player.createPlayer(center_position_x, center_position_y);
 
-    game.addOnStage(hero);
+
+    let i = 10;
+    while(i--){
+        let new_hero = player.createPlayer(center_position_x, center_position_y);
+        game.addOnStage(new_hero);
+    }
 }
 
 main();
