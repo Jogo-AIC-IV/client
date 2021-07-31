d7488978b9bde855f3c95eba38a51f718b2b32cd
unknown <Iago>
2021-07-31 17:11:51 -0300

    Muitas coisas mano

--- components/enemy.js
+++ components/enemy.js
@@ -43,6 +43,58 @@ class Enemy {
             });
         }
 
+        sprite.setComplexPath = function(game, paths, speed, callback_success = null, callback_died = null) {
+            this.current_path = 0;
+            this.x = paths[this.current_path].x;
+            this.y = paths[this.current_path].y;
+
+            game.ticker.add((time) => {
+                let position_start = paths[this.current_path];
+                let position_end = paths[this.current_path < paths.length - 1 ? this.current_path + 1 : 0];
+
+                if(this.current_path + 1 == paths.length){
+                    this.x = position_end.x;
+                    this.y = position_end.y;
+                    this.life_curr = this.life_max;
+                }
+
+                this.x += Math.sign(position_end.x - this.x) * speed;
+                this.y += Math.sign(position_end.y - this.y) * speed;
+
+                if(Math.abs(this.x - position_end.x) <= speed && Math.abs(this.y - position_end.y) <= speed){
+                    this.x = position_start.x;
+                    this.y = position_start.y;
+                }
+
+                if(this.life_curr != this.life_max){
+                    let tint_factor = this.life_curr/this.life_max;
+                    this.tint = PIXI.utils.rgb2hex([1, tint_factor - 1, tint_factor - 1]);
+                }else{
+                    this.tint = PIXI.utils.rgb2hex([1, 1, 1]);
+                }
+
+                
+                if(this.life_curr <= 0){
+                    this.life_curr = this.life_max;
+                    this.current_path = 0;
+                    this.x = paths[0].x;
+                    this.y = paths[0].y;
+                    if(typeof callback_died === 'function') { callback_died(); }
+                    return;
+                }
+
+                if(this.x == position_start.x && this.y == position_start.y){
+                    this.current_path = this.current_path < paths.length - 1 ? this.current_path + 1 : 0;
+                    this.x = paths[this.current_path].x;
+                    this.y = paths[this.current_path].y;
+                    if(typeof callback_success === 'function') { callback_success(); }
+                }
+
+            });
+        }
+
+
+
         return sprite;
     }
 }
--- components/init.js
+++ components/init.js
@@ -8,31 +8,76 @@ const TURRET    = new Tower();
 const ENEMY     = new Enemy();
 const game      = new Game(HTML_DOM);
 
-let money       = 300;
+const PATH = [{x:100, y:100}, {x:700, y:100}, {x:600, y:400}, {x:100, y:400}, {x:100, y:700}, {x:800, y:700}];
+
+let money       = 50;
+let enemy_value = 50;
 let money_counter = document.querySelector("#money_counter");
-let price  = 200;
-let price_counter = document.querySelector("#price_counter");
+let unit_price  = 50;
+let unit_price_counter = document.querySelector("#unit_price_counter");
+let damage_price = 10000;
+let damage_price_counter = document.querySelector("#damage_price_counter");
+
+function treat_number(number){
+    sufix = '';
+    while(number > 1000){
+        sufix += 'K';
+        number = number/1000;
+    }
+    return number.toFixed(2) + sufix;
+}
+
+damage_price_counter.innerHTML = 'R$' + treat_number(damage_price);
+unit_price_counter.innerHTML = 'R$' + treat_number(unit_price);
+money_counter.innerHTML = treat_number(money);
 
 function buyUnit(){
-    if(money >= price){
-        money -= price;
+    if(money >= unit_price){
+        money -= unit_price;
         let tower = TURRET.createTower({x: Math.random()*800, y: Math.random()*800}, 300);
         game.addTower(tower);
-        price *= 1.25;
-        price = price.toFixed();
-        price_counter.innerHTML = 'R$' + price;
-        money_counter.innerHTML = money;
+        unit_price *= 2.5;
+        unit_price_counter.innerHTML = 'R$' + treat_number(unit_price);
+        money_counter.innerHTML = treat_number(money);
+    }
+}
+
+function buyDamage(){
+    if(money >= damage_price){
+        money -= damage_price;
+        damage_price *= 10;
+        damage_price = damage_price.toFixed();
+        damage_price_counter.innerHTML = 'R$' + treat_number(damage_price);
+        money_counter.innerHTML = treat_number(money);
+
+        game.towers.forEach((tower) => {
+            tower.bullets.config.damage += 1;
+        });
+    }
+}
+
+function enemyDie() {
+    money += enemy_value;
+    money = Math.ceil(money);
+    enemy_value = enemy_value * 1.025;
+    money_counter.innerHTML = money;
+    if(game.enemies.length * 1.4 < game.towers.length){
+        let new_emeny = ENEMY.createEnemy({x: 500, y: 600}, 6);
+        new_emeny.setComplexPath(game, [{x:Math.random()*-200, y:100}, {x:600, y:100}, {x:600, y:400}, {x:100, y:400}, {x:100, y:600}, {x:800, y:600}], 3, null, enemyDie);
+        new_emeny.rotation = Math.PI/2;
+        game.addEnemy(new_emeny);
     }
 }
 
 function main() {
 
-    for(let i = 0; i < 10; i++) {
-        let enemy = ENEMY.createEnemy({x: 500, y: 600}, 3);
-        enemy.setLinearLoop(game, {x: 400, y: i*-100}, {x:400 , y:900}, 3, null, () => {
-            money += 100;
-            money_counter.innerHTML = money;
-        });
+    for(let i = 0; i < 1; i++) {
+        let enemy = ENEMY.createEnemy({x: 500, y: 600}, 6);
+        // enemy.setLinearLoop(game, {x: 400, y: i*-100}, {x:400 , y:900}, 3, null, () => {
+        //     money += 100;
+        //     money_counter.innerHTML = money;
+        // });
+        enemy.setComplexPath(game, [{x:i*-100, y:100}, {x:600, y:100}, {x:600, y:400}, {x:100, y:400}, {x:100, y:600}, {x:800, y:600}], 3, null, enemyDie);
         enemy.rotation = +Math.PI/2;
         game.addEnemy(enemy);
     }
